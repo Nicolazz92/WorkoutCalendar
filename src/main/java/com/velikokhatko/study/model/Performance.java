db4900342ff81c9ed565641c34d0d78b8f19d7f6
sbt-velikokhatko-nn <NNVelikokhatko.SBT@sberbank.ru>
2020-07-29 11:12:53 +0300

    Закрыл Performance возможность редактировать связь с UserProfile

--- src/main/java/com/velikokhatko/study/model/Performance.java
+++ src/main/java/com/velikokhatko/study/model/Performance.java
@@ -6,6 +6,7 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import java.time.LocalTime;
 
@@ -16,6 +17,7 @@ import java.time.LocalTime;
 public class Performance extends BaseEntity {
 
     @OneToOne(mappedBy = "performance")
+    @JoinColumn(name = "id", insertable = false, updatable = false)
     private UserProfile userProfile;
     private LocalTime run100m;
     private LocalTime run500m;
